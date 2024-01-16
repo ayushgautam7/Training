706492dc7a7a565700e98375cbb9e91db533f9ed
Ayush Gautam <gautamayush721@gmail.com>
2024-01-16 15:13:09 +0530

    mysql connection

---
+++ src/main/java/com/training/day5/jdbc/Connection_mysql.java
@@ -0,0 +1,40 @@
+package com.training.day5.jdbc;
+
+import com.mysql.cj.PerConnectionLRUFactory;
+
+import java.sql.*;
+
+public class Connection_mysql {
+    public static void main(String[] args) {
+       try{
+           Class.forName("com.mysql.cj.jdbc.Driver");
+       } catch(ClassNotFoundException e){
+           System.out.println(e);
+        }
+       Connection con = null;
+
+        try {
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/ukgTraining","root","password");
+            System.out.println("Connection is done");
+            Statement stmt=con.createStatement();
+            ResultSet rs=stmt.executeQuery("select * from emp");
+            while(rs.next())
+            {
+                System.out.println(rs.getInt(1)+": "+rs.getString(2)+": "+rs.getDouble(3));
+            }
+        } catch (SQLException e) {
+            System.out.println(e);
+        }catch (Exception e){
+            System.out.println(e);
+        }finally {
+            if (con !=null)
+                try{
+                    con.close();
+                }catch(SQLException e){
+                    System.out.println("Some error");
+                }
+        }
+
+    }
+
+}
